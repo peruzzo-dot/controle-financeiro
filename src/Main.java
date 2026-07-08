5b8c025808c6b0000904a52ed31af9f079e79b02
vitória <peruzzo509@gmail.com>
2026-07-08 18:22:39 -0300

    Exercício 2 - Área do quadrado

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
-import java.time.OffsetDateTime;
 import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String [] args) {
-        var baseYear = OffsetDateTime.now().getYear();
-        var scanner = new Scanner(System.in);
-        System.out.println("Informe o seu nome:");
-        var name = scanner.next();
-        System.out.println("Informe o seu ano de nascimento");
-        var year = scanner.nextInt();
-        var age = baseYear - year;
-        System.out.printf("Olá %s você tem %s anos \n", name, age);
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o tamanho do lado do quadrado:");
+
+        double lado = scanner.nextDouble();
+
+        double area = lado * lado;
+
+        System.out.println("A área do quadrado é: " + area);
 
     }
 }
